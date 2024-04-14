c015d3e79144557625cc36effbff8311c572ec5d
Luis J. Gonzalez <jamesqc999@gmail.com>
2024-04-14 10:17:53 -0500

    Start implementation at UI level

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import { getWeatherData } from "./service/weatherService";
+import getWeatherData from './service/weatherService';
+import './front/css/style.css';
 
 const form = document.querySelector('form');
 const city = document.getElementById('city');
 
 form.addEventListener('submit', (event) => {
-
-//   getWeatherData(city.value).then((response) => {
-//     console.log('index.js response: ');
-//     console.log(response);
-//   });
+  //   getWeatherData(city.value).then((response) => {
+  //     console.log('index.js response: ');
+  //     console.log(response);
+  //   });
   getResponseFromAPI();
   event.preventDefault();
 });
 
-async function getResponseFromAPI(){
-    const response = await getWeatherData(city.value);
-    console.log('outside of the then: ');
-    console.log(response);
+async function getResponseFromAPI() {
+  const response = await getWeatherData(city.value);
+  console.log('index.js outside of the then: ');
+  console.log(response);
 }
--- src/service/weatherService.js
+++ src/service/weatherService.js
@@ -1,16 +1,16 @@
-import { getWeatherLocation } from "../api/weather";
+import getWeatherLocation from '../api/weather';
 
-async function getWeatherData(city){
-    const weatherData = await getWeatherLocation(city);
-    const response = {
-        'city': weatherData.location.name,
-        'country' : weatherData.location.country,
-        'feelsLike_c' : weatherData.current.feelslike_c,
-        'temp_c' : weatherData.current.temp_c,
-        'is_day' : weatherData.current.is_day === 1 ? true : false
-    }
+async function getWeatherData(city) {
+  const weatherData = await getWeatherLocation(city);
+  const response = {
+    city: weatherData.location.name,
+    country: weatherData.location.country,
+    feelsLike_c: weatherData.current.feelslike_c,
+    temp_c: weatherData.current.temp_c,
+    is_day: weatherData.current.is_day === 1,
+  };
 
-    return response;
+  return response;
 }
 
-export {getWeatherData}
+export default getWeatherData;
